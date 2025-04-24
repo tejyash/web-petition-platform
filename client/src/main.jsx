e83100c6aa026ffa74715d3cc9b2cb9657b07959
tejyash <tejyashtrilok@gmail.com>
2025-04-24 16:20:08 +0100

    updated main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import PetitionerDashboard from './components/petitioner-dashboard';
 import PetitionCommittee from './components/petitioner-commitee';
@@ -8,13 +8,13 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/petition" element={<PetitionerDashboard />} />
         <Route path="/petitioner/dashboard" element={<PetitionerDashboard />} />
         <Route path="/committee/dashboard" element={<PetitionCommittee />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
